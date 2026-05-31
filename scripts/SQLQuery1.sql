70d97176fc23f8a9a3689c6a02c0713a867489d9
Mohammed Ayan Yezdani <93386903+lordpan1k@users.noreply.github.com>
2026-05-31 15:18:30 +0530

    Modify SQL script to handle existing database
    Added logic to drop existing DataWarehouse database before creation.

--- scripts/SQLQuery1.sql
+++ scripts/SQLQuery1.sql
@@ -1,11 +1,28 @@
+
 USE master;
-Create DATABASE DataWarehouse;
-Use DataWarehouse;
+GO
+
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+GO
+
+-- Creating the 'DataWarehouse' database
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+-- Create Schemas
 CREATE SCHEMA bronze;
 GO
 
-CREATE SCHEMA Silver;
+CREATE SCHEMA silver;
 GO
-CREATE SCHEMA GOLD;
+
+CREATE SCHEMA gold;
 GO
 
